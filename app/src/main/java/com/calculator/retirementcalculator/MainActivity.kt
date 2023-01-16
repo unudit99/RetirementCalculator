90bc13d8c94b68f89ba05bdf62285661955f4bb2
Udit Narayan <udit.narayan@kelltontech.org>
2023-01-16 15:31:56 +0530

    addd crash

--- app/src/main/java/com/calculator/retirementcalculator/MainActivity.kt
+++ app/src/main/java/com/calculator/retirementcalculator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.calculator.retirementcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
@@ -12,5 +13,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //this line of code is enogh to get crash analaytics
         AppCenter.start(application,"71559eab-d953-4c32-88dd-74bb8cc12664",Analytics::class.java,Crashes::class.java)
+    }
+
+    fun HelloClick(view: View) {
+        //throw Exception("throw exception")
+        Crashes.generateTestCrash()
     }
 }
